5de9428d88da4600971e96523ad791dc9c91404e
Abraham <96961155+danni-boi-dot@users.noreply.github.com>
2023-04-12 23:43:30 -0600

    se agrego el formulario para el registro de clientes

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -5,9 +5,25 @@ function Cliente() {
     const { nombre, empresa, email, telefono, id } = cliente
     return (
         <tr className='border-b'>
-            <td className="p-6">
+            <td className="p-6 space-y-2">
                 <p className='text-2xl text-gray-800'>{nombre}</p>
                 <p>{empresa}</p>
+            </td>
+            <td className='p-6'>
+                <p className='text-gray-600'> <span className='text-gray-800 uppercase font-bold'>Email: </span>{email}</p>
+                <p className='text-gray-600'> <span className='text-gray-800 uppercase font-bold'>Telefono: </span>{telefono}</p>
+            </td>
+            <td className='p-6 flex gap-3'>
+                <button type='button'
+                    className='text-slate-800 hover:text-slate-950 uppercase font-bold text-xs'
+                >
+                    Editar
+                </button>
+                <button type='button'
+                    className='text-red-800 hover:text-red-950 uppercase font-bold text-xs'
+                >
+                    Eliminar
+                </button>
             </td>
         </tr>
     )
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario"
 
-const NuevoCliente = () => {
+function NuevoCliente() {
+
+  const navigate = useNavigate()
   return (
-    <div>
-        
-    </div>
+    <>
+      <h1 className='font-black text-4xl text text-slate-800'>Nuevo Cliente</h1>
+      <p className='mt-3'>Llena todos los campos para registrar un nuevo cliente</p>
+      <div className='flex justify-end '>
+        <button
+          className='bg-mamon text-white px-3 py-1'
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            className="mt-5 w-full bg-mamon p-3 uppercase font-bold text-white text-lg"
+            value='Registrar cliente'
+          />
+        </form>
+      </div>
+    </>
   )
 }
 
