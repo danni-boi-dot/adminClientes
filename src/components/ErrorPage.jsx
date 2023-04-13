859c58cbf14b7df717f6036efd04ba79a4d95369
Abraham <96961155+danni-boi-dot@users.noreply.github.com>
2023-04-13 01:56:54 -0600

    Se puso la funcionalidad de editar

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -6,7 +6,7 @@ function ErrorPage() {
     const error = useRouteError()
     return (
         <div className="space-y-8">
-            <h1 className="text-center text-6xl font-extrabol mt-20 text-slate-900"> pilin</h1>
+            <h1 className="text-center text-6xl font-extrabol mt-20 text-slate-900"> Erores</h1>
             <p className="text-center">Hubo un Herror</p>
             <p className="text-center">{error.statusText || error.message}</p>
         </div>
